5109a86de7c31580c87edb8324bdbfb243545aa8
On1-zuka <kuleshilja@yandex.by>
2025-01-27 18:49:03 +0300

    add github tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,1 +1,9 @@
-print("test")
+from src.utils import add_numbers, multiply_numbers
+
+
+def test_add_numbers(a, b):
+    assert add_numbers(2, 4) == 6
+
+
+def test_multiply_numbers(a, b):
+    assert multiply_numbers(2, 2) == 4
